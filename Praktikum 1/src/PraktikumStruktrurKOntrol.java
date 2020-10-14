d5bd0b20676c51b0a6a946e8a8b61fb8c5c723e1
raflimuslim1927 <72854621+raflimuslim1927@users.noreply.github.com>
2020-10-14 14:31:33 +0700

    Add files via upload

---
+++ Praktikum 1/src/PraktikumStruktrurKOntrol.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class PraktikumStruktrurKOntrol {
+    public static void main(String[] args) {
+        int umur =(18);
+        
+        if(umur>=17){
+            System.out.println("anda diperbolehkan memiliki SIM")
+                    
+                
+            
+          
+                            }           
+                  
+                  
+        }
+    }
+}
